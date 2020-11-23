b17cb800711ba5e00239785c66425b91df0b4596
zak <codedpills@gmail.com>
2020-11-23 11:55:31 +0000

    add action creators for edit and delete

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -1,11 +1,24 @@
 //action creator for adding user
 export const addUser = (newUser) => {
-    newUser.id = Math.random().toString();
-    return {
-        type: "ADD_USER",
-        payload: newUser
-    }
-}
+  newUser.id = Math.random().toString();
+  return {
+    type: "ADD_USER",
+    payload: newUser,
+  };
+};
 
+//action creator for deleting user
+export const deleteUser = (userId) => {
+  return {
+    type: "DELETE_USER",
+    payload: userId,
+  };
+};
 
-
+//action creator for editing user
+export const editUser = (updatedUser) => {
+  return {
+    type: "EDIT_USER",
+    payload: updatedUser,
+  };
+};
