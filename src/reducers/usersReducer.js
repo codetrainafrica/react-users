a6d37b538489484acc303b10787855a755ca44d0
zak <codedpills@gmail.com>
2020-11-23 11:55:58 +0000

    add edit and delete functionality

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -22,14 +22,20 @@ const initialState = {
 };
 
 const usersReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case "ADD_USER":
-                console.log(action.payload)
-                return {...state, users: [...state.users, action.payload]};
-    
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "ADD_USER":
+      console.log(action.payload);
+      return { ...state, users: [...state.users, action.payload] };
+    case "DELETE_USER":
+      let undeletedUsers = state.users.filter((user) => user.id !== action.payload);
+      return {...state, users: undeletedUsers}
+    case "EDIT_USER":
+      return {...state, users: state.users.map((user) =>
+        user.id === action.payload.id ? action.payload : user
+      ),}
+    default:
+      return state;
+  }
 };
 
 export default usersReducer;
