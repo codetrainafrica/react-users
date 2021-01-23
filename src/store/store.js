9934b3fe3efbdd900f6407f7095197838585c1b0
zak <codedpills@gmail.com>
2021-01-23 14:46:29 +0000

    add firebase config to redux store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,23 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import usersReducer from "../reducers/usersReducer";
 import authReducer from "../reducers/authReducer";
 
+// new imports
+import firebase from '../firebase/firebase';
+import { getFirebase, reactReduxFirebase} from 'react-redux-firebase';
+import { getFirestore, reduxFirestore} from 'redux-firestore';
+import thunk from 'redux-thunk';
+
+const reducers = combineReducers({
+  usersState: usersReducer,
+  auth: authReducer,
+})
+
 export const store = createStore(
-  combineReducers({
-    usersState: usersReducer,
-    auth: authReducer,
-  })
+  reducers,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+    reactReduxFirebase(firebase),
+    reduxFirestore(firebase)
+  )
 );
