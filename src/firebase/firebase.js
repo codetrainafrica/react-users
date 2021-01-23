3baa0fb19ade14dc2ce5cc776b524e6b9810172d
zak <codedpills@gmail.com>
2021-01-23 10:04:27 +0000

    add firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,13 @@
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCz1BQfAq9CjA2Ro8U3KtprmMXHDicvko8",
+    authDomain: "react-users-40d41.firebaseapp.com",
+    projectId: "react-users-40d41",
+    storageBucket: "react-users-40d41.appspot.com",
+    messagingSenderId: "473365738309",
+    appId: "1:473365738309:web:0b7a9087ea7cda8d24042c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
